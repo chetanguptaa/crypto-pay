0687bcfdd1bff454942d25463be769639c54b0c3
chetan gupta <chetangupta123raj@gmail.com>
2023-08-14 21:03:47 +0530

    updated components elements

--- client/src/components/AccountDetails.tsx
+++ client/src/components/AccountDetails.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
-const AccountDetails = () => {
+import { Card } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+function AccountDetails({ address, name, balance }) {
   return (
-    <div>AccountDetails</div>
-  )
+    <Card title="Account Details" style={{ width: "100%" }}>
+      <div className="accountDetailRow">
+        <UserOutlined style={{ color: "#767676", fontSize: "25px" }} />
+        <div>
+          <div className="accountDetailHead"> {name} </div>
+          <div className="accountDetailBody">
+            {" "}
+            Address: {address.slice(0, 4)}...{address.slice(38)}
+          </div>
+        </div>
+      </div>
+      <div className="accountDetailRow">
+        <div>
+          <div className="accountDetailHead"> Native Matic Tokens</div>
+          <div className="accountDetailBody">{balance} Matic</div>
+        </div>
+      </div>
+      <div className="balanceOptions">
+        <div className="extraOption">Set Username</div>
+        <div className="extraOption">Switch Accounts</div>
+      </div>
+    </Card>
+  );
 }
 
-export default AccountDetails
+export default AccountDetails;
--- client/src/components/CurrentBalance.tsx
+++ client/src/components/CurrentBalance.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
-const CurrentBalance = () => {
+import { Card } from "antd";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+function CurrentBalance({dollars}) {
   return (
-    <div>CurrentBalance</div>
-  )
+    <Card title="Current Balance" style={{ width: "100%" }}>
+      <div className="currentBalance">
+        <div style={{ lineHeight: "70px" }}>$ {dollars}</div>
+        <div style={{ fontSize: "20px" }}>Available</div>
+      </div>
+      <div className="balanceOptions">
+        <div className="extraOption">Swap Tokens</div>
+        <div className="extraOption">Bridge Tokens</div>
+      </div>
+    </Card>
+  );
 }
 
-export default CurrentBalance
+export default CurrentBalance;
